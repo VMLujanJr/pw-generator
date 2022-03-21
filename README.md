a65212020653103089ea64a8f9ca9ce711827aae
Manny <vmlujanjr@outlook.com>
2022-03-20 19:25:01 -0500

    finalized password generator

--- README.md
+++ README.md
@@ -18,53 +18,53 @@ SO THAT I can create a strong password that provides greater security
 
 GIVEN I need a new, secure password
 
-1. WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
+1. ~~WHEN I click the button to generate a password~~
+~~THEN I am presented with a series of prompts for password criteria~~
 
-2. WHEN prompted for password criteria
-THEN I select which criteria to include in the password
+2. ~~WHEN prompted for password criteria~~
+~~THEN I select which criteria to include in the password~~
 
-3. WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters 
+3. ~~WHEN prompted for the length of the password~~
+~~THEN I choose a length of at least 8 characters and no more than 128 characters~~
 
-4. WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+4. ~~WHEN asked for character types to include in the password~~
+~~THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters~~
 
-5. WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
+5. ~~WHEN I answer each prompt~~
+~~THEN my input should be validated and at least one character type should be selected~~
 
-6. WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
+6. ~~WHEN all prompts are answered~~
+~~THEN a password is generated that matches the selected criteria~~
 
-7. WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+7. ~~WHEN the password is generated~~
+~~THEN the password is either displayed in an alert or written to the page~~
 
 ### **Additional Criteria**
 
 **Technical**
 
-- Satisfies all of the preceding acceptance criteria
+- ~~Satisfies all of the preceding acceptance criteria~~
 
 **Deployment**
 
-- Application deployed at live URL
-- Application loads with no errors
-- Application GitHub URL submitted
-- GitHub repository contains application code
+- ~~Application deployed at live URL~~
+- ~~Application loads with no errors~~
+- ~~Application GitHub URL submitted~~
+- ~~GitHub repository contains application code~~
 
 **Application Quality**
 
-- Application user experience is intuitive and easy to navigate
-- Application user interface style is clean and polished
-- Application resembles the mock-up functionality
+- ~~Application user experience is intuitive and easy to navigate~~
+- ~~Application user interface style is clean and polished~~
+- ~~Application resembles the mock-up functionality~~
 
 **Repository Quality**
 
-- Repository has a unique name
-- Repository follows best practices for file structure and naming conventions
-- Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-- Repository contains multiple descriptive commit messages
-- Repository contains quality README.md with description, screenshots, link to deployed application
+- ~~Repository has a unique name~~
+- ~~Repository follows best practices for file structure and naming conventions~~
+- ~~Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.~~
+- ~~Repository contains multiple descriptive commit messages~~
+- ~~Repository contains quality README.md with description, screenshots, link to deployed application~~
 
 ### **Sample Password Generator**
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,85 @@
-var generatePassword = function() {
-  if (password === "" || password === null) {
-    window.alert = ("Welcome to your Password Generator!\n\nLet's make a 💪 & 🔐 password!");
-  }
-}
-var pass = [];
+// *********************
+// VARIABLE DECLARATIONS
+// *********************
+/* Four Types of Characters */
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var integers = "0123456789";
+var symbols = "!@#$%^&*()_+?><:{}[]";
 
-for (var randomLow = 0; randomLow < 8; randomLow++) {
-    pass.push(randomLow);
-}
-console.log(pass);
+/* Empty / Filled Password Array */
+var uniquePassword = {
+  emptyArray: "",
+  reset: function() {
+    this.emptyArray = "";
+  }
+};
+
+// *********************
+// COMBINES CHARACTERS TOGETHER
+// *********************
+var combineCharacters = function() {
+  /* Collects Character Type Arrays into a single array */
+  var createCharArray = [];
+  
+  /* Character Type Confirmation */
+  var acceptLow = confirm("Want us to include lower case letters?");
+  var acceptUpp = confirm("Want us to include upper case letters?");
+  var acceptInt = confirm("Want us to include numbers?");
+  var acceptSym = confirm("Want us to include symbols?");
+  
+  /* If no character type is chosen, you will be prompted to try again */
+  if (acceptLow || acceptUpp || acceptInt || acceptSym) {
+    /* If you choose at least one character type, these if statements will concantenate and create one array. */
+    if (acceptLow) {
+      createCharArray = createCharArray.concat(lowerCase.split(""));
+    }
+    if (acceptUpp) {
+      createCharArray = createCharArray.concat(upperCase.split(""));
+    }
+    if (acceptInt) {
+      createCharArray = createCharArray.concat(integers.split(""));
+    }
+    if (acceptSym) {
+      createCharArray = createCharArray.concat(symbols.split(""));
+    }
+  } else {
+    alert("You did not accept any character types! Please try again!");
+
+    return combineCharacters();
+  }
+
+  return createCharArray;
+};
+
+// *********************
+// GENERATES PASSWORD
+// *********************
+var initGenerator = function() {
+  // reset password to empty before creating a new one
+  uniquePassword.reset();
+
+  var arrayOfCharacters = combineCharacters();
+
+  /* Character Length Prompt / convert text to integers */
+  var howManyChars = prompt("How many characters? You may type a number between 8 and 128 characters!");
+  var generateChars = parseInt(howManyChars);
+
+  /* Verifies if the character length was below 8, or higher than 128! */
+  if (generateChars >= 8 && generateChars <= 128) {
+    if (uniquePassword.emptyArray === null || uniquePassword.emptyArray === "") {
+        for (var i = 0; i < generateChars; i++) {
+        var randomIndex = Math.floor(Math.random() * arrayOfCharacters.length);
+        uniquePassword.emptyArray += arrayOfCharacters[randomIndex];
+      }
+    } else {
+      alert("The unique password did not reset before creating a new unique password!");
+    }
+  } else {
+    alert("You typed in a number below 8 or higher than 128! Try again!");
+  }
+  return uniquePassword.emptyArray
+};
 
 // ************************
 // DO NOT CHANGE CODE BELOW
@@ -19,34 +90,11 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = initGenerator();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// *********************
-// VARIABLE DECLARATIONS
-// *********************
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v","w", "x","y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P","Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var symbols = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-// *********************
-// RANDOM GENERATORS
-// *********************
-var randomLow = lowerCase[Math.floor(Math.random() * lowerCase.length)];
-console.log(randomLow);
-var randomUpp = upperCase[Math.floor(Math.random() * upperCase.length)];
-console.log(randomUpp);
-var randomInt = integers[Math.floor(Math.random() * integers.length)];
-console.log(randomInt);
-var randomSym = symbols[Math.floor(Math.random() * symbols.length)];
-console.log(randomSym);
-
-generatePassword();
